4aa588a280a280c2dea48ba8fe0a6d618c4996df
Pablo Andres Chacon <pabloandres120297@gmail.com>
2025-08-13 20:48:51 -0500

    finished register form

--- src/app/components/Auth/RegisterForm/RegisterForm.js
+++ src/app/components/Auth/RegisterForm/RegisterForm.js
@@ -6,7 +6,13 @@ export function RegisterForm() {
     <Form>
       <Form.Group widths="equal">
         <Form.Input name="email" type="text" placeholder="Correo electrónico" />
+        <Form.Input name="username" type="text" placeholder="Nombre del usuario" />
       </Form.Group>
+      <Form.Group widths="equal">
+        <Form.Input name="name" type="text" placeholder="Nombre y apellidos" />
+        <Form.Input name="password" type="password" placeholder="Contraseña" />
+      </Form.Group>
+      <Form.Button type="submit" fluid>Registrarse</Form.Button>
     </Form>
   )
 }
