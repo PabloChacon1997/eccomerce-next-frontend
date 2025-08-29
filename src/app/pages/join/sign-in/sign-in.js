bd5c8fb23624d626f22a3c817ef9961b38433b14
Pablo Andres Chacon <pabloandres120297@gmail.com>
2025-08-28 20:22:40 -0500

    login user

--- src/app/pages/join/sign-in/sign-in.js
+++ src/app/pages/join/sign-in/sign-in.js
@@ -1,5 +1,7 @@
+import Link from "next/link"
 import styles from "./sign-in.module.scss"
 import { JoinLayout } from "@/app/layouts"
+import { LoginForm } from "@/app/components/Auth"
 
 export default function SignInPage() {
   return (
@@ -7,6 +9,10 @@ export default function SignInPage() {
       <JoinLayout>
         <div className={styles.signIn}>
           <h3>Iniciar Sesión</h3>
+          <LoginForm />
+          <div className={styles.actions}>
+            <Link href="/join/sign-up">¿No tienes una cuenta?</Link>
+          </div>
         </div>
       </JoinLayout>
     </>
